8617215fba4c79c92694725007c4e3be5e12f487
Boris Sukhovilov <boris.sukhovilov@gmail.com>
2024-12-21 21:34:32 +0500

    #

--- SphereRadius_Sukhovilov.m
+++ SphereRadius_Sukhovilov.m
@@ -1,5 +1,5 @@
 %  R = 1 / sqrt(sum(sum(pinv(S2))));
-function R = circumscribedSphereRadius_Sukhovilov(S)
+function [R, x, sigma_R] = SphereRadius_Sukhovilov(S, sigma, sigma_m)
     % ѕостроение матрицы S2
     S2 = 0.5 * S.^2;
     rank_S2_0 = 4;
@@ -14,4 +14,15 @@ function R = circumscribedSphereRadius_Sukhovilov(S)
     L_inv = diag(1 ./ diag(L(1:rank_S2, 1:rank_S2)));
     S2_pinv = V(:, 1:rank_S2) * L_inv * U(:, 1:rank_S2)';
     R = 1 / sqrt(sum(sum(S2_pinv)));
+    
+    b = ones(size(S2,1),1);
+    x = S2_pinv*b;
+    
+
+    % Calculate RMSE of radius R - sigma_R
+    d=(2*sigma^2)*((x.^2)'*S2*(x.^2));      % dispersion (1/R^2)
+    % составл€юща€ от ошибок модели
+    d=d+4*sigma_m^2*x'*x/R^2;
+    sigma_R=0.5*(R^3)*sqrt(d);        % root mean square error (RMSE) R
+
 end
