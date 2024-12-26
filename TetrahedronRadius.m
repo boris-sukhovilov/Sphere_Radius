6234e47e98031a403a9a81edc708b931b178d9e0
Boris Sukhovilov <boris.sukhovilov@gmail.com>
2024-12-26 22:19:15 +0500

    #

--- TetrahedronRadius.m
+++ TetrahedronRadius.m
@@ -2,36 +2,61 @@ function TetrahedronRadius()
     clc
 
     R = 1000;
-    numPoints = 4;
+%     numPoints = 100;
     
-    h = R*0.001;   % Расстояние от центра сферы до горизонтальных плоскостей, расположения точек
+    h = R*0.98;   % Расстояние от центра сферы до горизонтальных плоскостей, расположения точек
+%     h = R*0.999;   % Расстояние от центра сферы до горизонтальных плоскостей, расположения точек
     h
     
     theta = pi / 4; % Угол между диаметральными плоскостями
 
     type_distrib = 1;
     delta = 0.1;
+    sigma_m  = 1.;
     
     thetaRange = [0 2*pi];
     phiRange = [0 pi/6];
     
     rng('shuffle');
 
-    generate_type = 1;
-    
-    if generate_type == 0
-        points = generateRandomPointsInSolidAngle(R, numPoints, thetaRange, phiRange, sigma_form);
-    else
-        points = generate_optim_tetrahedron_points(R, h, theta);
-    end
+%     generate_type = 1;
+%     
+%     if generate_type == 0
+%         points = generateRandomPointsInSolidAngle(R, numPoints, thetaRange, phiRange, sigma_m);
+%     else
+        points = generate_optim_tetrahedron_points(R, h, theta, sigma_m);
+%     end
 
     % Генерация матрицы расстояний
-    [S, S0, sigma] = generateMatrixDistance(points, delta, type_distrib);
+    [S, ~, sigma] = generateMatrixDistance(points, delta, type_distrib);
 %     S0
 %     S
 
-    R_Sukhovilov = SphereRadius_Sukhovilov(S);
-    fprintf('Радиус описанной сферы, вычисленный по формуле Суховилова: %g\n', R_Sukhovilov);
+    [R1, ~, sigma_R] = SphereRadius_Sukhovilov(S, sigma, sigma_m);
+    fprintf('Радиус описанной сферы, вычисленный 1-м методом: %g\n', R1);
+    fprintf('RMSE of R: %g\n\n', sigma_R);
+%     x'
+%     1./sqrt(sum(x))
+
+    rmin0 = R1 - 4*sigma_R; 
+    rmax0 = R1 + 4*sigma_R;
+    rmin0 = max([eps, rmin0]);
+    rmin0, rmax0
+    
+    % Минимальный размер интервала изоляции корня 
+    d = 3*sqrt(sigma^2 + sigma_m^2)/1000;
+
+    [R2, sigma_R, sigma_max, status] = SphereRadius_Sukhovilov2(R1, S, sigma, sigma_m, rmin0, rmax0, d);
+    if status == 1
+        for i = 1 : length(R2)
+            fprintf('Metod 2 Radius: %g\n', R2(i));
+            fprintf('RMSE of R: %g\n', sigma_R(i));
+            fprintf('Upper bound for RMSE of R: %g\n', sigma_max(i));
+        end
+    else
+        fprintf('Metod 2 Radius not found!\n');
+    end
+    
 
     % Получим ребра тетраэдра
     [a, b, c, a1, b1, c1] = getTetrahedronEdges(S);
---
+++ example_for_suh.m
@@ -0,0 +1,132 @@
+function example_for_suh()
+clc
+
+sigma = 0.03/3;
+
+sigma_m = 0.0;
+
+format long
+
+% пример для Суховилова, что около полюсов располагать точки нежелательно
+% малые ошибки измерений вызывают большую ошибку радиуса
+% example_for_suh
+
+r=@(d)1/sqrt( sum(sum( inv(  [0 d(1:3); d(1) 0 d(4:5); d(2) d(4) 0 d(6);...
+    d(3) d(5) d(6) 0].^2/2))));
+
+% исх данные расчётов
+phi=0.0988; % Угол отклонения от полюса
+% phi=0.2; % Угол отклонения от полюса
+relerr=0.01*[1,1,0,0,0,1];     % задаём относительную погрешность измерений
+
+disp('пример, что малые ошибки измерений вызывают большую ошибку радиуса')
+disp(['угол отклонения от полюса  ',num2str(phi)])
+disp('относительные погрешности для измерений')
+disp(relerr)
+
+% задаю координаты точек по предложению Суховилова
+M1=[ sin(phi); 0;         cos(phi)]; % сев полюс направо
+M2=[-sin(phi); 0;         cos(phi)];  % сев полюс налево
+M3=[0;          sin(phi); -cos(phi)]; % юж полюс от нас
+M4=[0;         -sin(phi); -cos(phi)]; % юж полюс к нам
+%  расстояния между точками с маш погрешностью
+s=zeros(1,6);
+s(1)=sqrt(     sum(   (M1-M2).^2   )         );%12
+s(2)=sqrt(     sum(   (M1-M3).^2   )         );%13
+s(3)=sqrt(     sum(   (M1-M4).^2   )         );%14
+s(4)=sqrt(     sum(   (M2-M3).^2   )         );%23
+s(5)=sqrt(     sum(   (M2-M4).^2   )         );%24
+s(6)=sqrt(     sum(   (M3-M4).^2   )         );%34
+disp('"точные" расстояния'); disp( num2str(s));
+
+disp(['радиус по "точным" расстояниям (анонимная функция):',num2str(r(s))])
+
+% S = [0   s(1) s(2) s(3);
+%     s(1) 0    s(4) s(5);
+%     s(2) s(4) 0    s(6);
+%     s(3) s(5) s(6) 0];
+
+% [U,S,V] = svd(S)
+% [V,D] = eig(S)
+
+
+% disp(['радиус по "точным" расстояниям   ',num2str(calculate_radius_Sukhovilov(S))])
+
+s=s.*(1+relerr); % измерения спогрешностью
+
+% s(2) = 0.5*(s(2)+s(5));
+% s(5)=s(2);
+% s(1) = 0.5*(s(1)+s(6));
+% s(6)=s(1);
+
+S = [0    s(1) s(2) s(3);
+     s(1) 0    s(4) s(5);
+     s(2) s(4) 0    s(6);
+     s(3) s(5) s(6) 0];
+
+tmp = (S(1,2) + S(3,4))/2;
+% tmp = 0;
+S(1,2) = tmp; 
+S(3,4) = tmp; 
+S(2,1) = tmp; 
+S(4,3) = tmp; 
+
+tmp = (S(1,4) + S(2,3))/2;
+S(1,4) = tmp; 
+S(2,3) = tmp; 
+S(4,1) = tmp; 
+S(3,2) = tmp; 
+
+tmp = (S(1,3) + S(2,4))/2;
+S(1,3) = tmp; 
+S(2,4) = tmp; 
+S(3,1) = tmp; 
+S(4,2) = tmp; 
+
+% [U,S,V] = svd(ss)
+% [V,D] = eig(ss)
+
+disp('измеренные расстояния'); disp( num2str(s));
+
+% disp(['радиус по измеренным расстояниям   ',num2str(calculate_radius_Sukhovilov(ss))])
+
+    [R1, x, sigma_R] = SphereRadius_Sukhovilov(S, sigma, sigma_m);
+    fprintf('Радиус описанной сферы, вычисленный 1-м методом: %g\n', R1);
+    fprintf('RMSE of R: %g\n\n', sigma_R);
+%     x'
+%     1./sqrt(sum(x))
+
+    rmin0 = R1 - 3*sigma_R; 
+    rmax0 = R1 + 3*sigma_R;
+    rmin0 = max([eps, rmin0]);
+    rmin0, rmax0
+    
+    % Минимальный размер интервала изоляции корня
+    d = 3*sqrt(sigma^2 + sigma_m^2)/1000;
+
+    [R2, sigma_R, sigma_max, status] = SphereRadius_Sukhovilov2(R1, S, sigma, sigma_m, rmin0, rmax0, d);
+    if status == 1
+        for i = 1 : length(R2)
+            fprintf('Metod 2 Radius: %g\n', R2(i));
+            fprintf('RMSE of R: %g\n', sigma_R(i));
+            fprintf('Upper bound for RMSE of R: %g\n', sigma_max(i));
+            sm = sensitivity_matrix(R2(i), S);
+            disp(sm);
+        end
+    else
+        fprintf('Metod 2 Radius not found!\n');
+    end
+
+%     disp(['радиус по измеренным расстояниям (анонимная функция):',num2str(r(s))])
+
+    % Получим ребра тетраэдра
+    [a, b, c, a1, b1, c1] = getTetrahedronEdges(S);
+    
+    % Вычисление радиуса через определитель Кэли-Менгера: https://ru.wikipedia.org/wiki/Симплекс
+    R_Cayley_Menger = circumscribedSphereRadius_Cayley_Menger(a, b, c, a1 , b1, c1);
+    fprintf('Радиус описанной сферы, вычисленный через определители Кэли-Менгера: %g\n', R_Cayley_Menger);
+
+    R_Grelle = circumscribedSphereRadius_Grelle(a, b, c, a1, b1, c1);
+    fprintf('Радиус описанной сферы, вычисленный по формуле Grelle: %g\n', R_Grelle);
+
+end
